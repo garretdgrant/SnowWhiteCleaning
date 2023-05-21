4cf97b8ebc5d188443e64df202195e1e1a780794
Garret G <g.grant92@gmail.com>
2023-05-20 17:28:27 -0700

    Nav created

---
+++ swcleaning/src/App.js
@@ -0,0 +1,15 @@
+import './index.css'
+import Navigation from "./components/Navigation";
+
+const App = () => {
+  return (
+    <>
+      <Navigation />
+      <h1>Snow White Cleaning</h1>
+    </>
+  );
+}
+
+
+
+export default App;
---
+++ swcleaning/src/components/Navigation/index.js
@@ -0,0 +1,24 @@
+import './Navigation.css'
+
+const Navigation = () => {
+    return (
+        <>
+            <div class="nav-bar">
+                
+                <div class="nav-left-side">
+                    Hamburger Menu
+                </div>
+                
+                <div class="nav-center">
+                    Snow White Cleaning
+                </div>
+                
+                <div class="nav-right-side">
+                    Contact
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Navigation;
